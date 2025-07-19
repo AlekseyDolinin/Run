a298e4613494d5d72e33e72a30cbabfb5a9effbb
allDolinin <allDolinin@gmail.com>
2025-07-19 22:07:20 +0300

    set get data from HealthKit

--- Run/App/TraningDetailView.swift
+++ Run/App/TraningDetailView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import Voyager
 
 struct TraningDetailView: View {
-        
+    
     @State private var vm = ViewModel()
     
     var body: some View {
@@ -39,8 +39,46 @@ extension TraningDetailView {
     class ViewModel {
         
         func getSteps() {
-            HealthKitManager.shared.getSteps { count in
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+            
+            let startString = "2025-07-18T18:17:00"
+            let startDate: Date = dateFormatter.date(from: startString)!
+            
+            let finishStaring = "2025-07-18T18:55:43"
+            let finishDate: Date = dateFormatter.date(from: finishStaring)!
+            
+            print("startDate: \(startDate)")
+            print("finishDate: \(finishDate)")
+            
+            HealthKitManager.shared.getSteps(start: startDate, finish: finishDate) { count in
                 print("count steps: \(count)")
+                let height = 1.77
+                let lenghtStep = height / 100.0 * 55
+                print("lenghtStep: \(lenghtStep)")
+                let distance = count * lenghtStep
+                print("distance: \(distance)")
+            }
+            
+            HealthKitManager.shared.getHeartRate(start: startDate, finish: finishDate) { count in
+                print("HeartRate: \(count)")
+            }
+            HealthKitManager.shared.getHeight { height in
+                print("height: \(height)")
+            }
+            HealthKitManager.shared.getBodyMass { mass in
+                print("mass: \(mass)")
+            }
+            HealthKitManager.shared.getRunningStrideLength(start: startDate, finish: finishDate) { runningStrideLength in
+                print("runningStrideLength: \(runningStrideLength)")
+            }
+            HealthKitManager.shared.getActiveEnergyBurned(start: startDate, finish: finishDate) { energy in
+                print("energy: \(energy)")
+            }
+            HealthKitManager.shared.getActiveEnergyBurned(start: startDate, finish: finishDate) { kilocalorie in
+                print("kilocalorie: \(kilocalorie)")
             }
         }
     }
--- Run/Services/HealthKit.swift
+++ Run/Services/HealthKit.swift
@@ -9,6 +9,7 @@ final class HealthKitManager: NSObject {
     var manager: HKHealthStore!
     
     let stepCount = HKObjectType.quantityType(forIdentifier: .stepCount)!
+    let height = HKObjectType.quantityType(forIdentifier: .height)!
     
     override init() {
         super.init()
@@ -29,25 +30,69 @@ final class HealthKitManager: NSObject {
     }
     
     func getHealthKitPermission() {
+        let healthKitTypesToRead: Set<HKSampleType> = [
+            HKSampleType.quantityType(forIdentifier: .height)!,
+            HKSampleType.quantityType(forIdentifier: .bodyMass)!,
+            HKSampleType.quantityType(forIdentifier: .stepCount)!,
+            HKSampleType.quantityType(forIdentifier: .heartRate)!,
+            HKSampleType.quantityType(forIdentifier: .activeEnergyBurned)!,
+            HKSampleType.quantityType(forIdentifier: .runningStrideLength)!
+        ]
         Task(priority: .userInitiated) {
             try await manager.requestAuthorization(
                 toShare: [],
-                read: [stepCount]
+                read: healthKitTypesToRead
             )
         }
     }
+}
+
+
+extension HealthKitManager {
     
-   func getSteps(completion: @escaping (Double) -> Void) {
-        let stepsQuantityType = HKQuantityType(.stepCount)
-        let now = Date()
-        let startOfDay = Calendar.current.startOfDay(for: now)
+    func getHeight(completion: @escaping (Double?) -> Void) {
+        let query = HKSampleQuery(
+            sampleType: HKSampleType.quantityType(forIdentifier: .height)!,
+            predicate: nil,
+            limit: 1,
+            sortDescriptors: nil
+        ) { (query, results, error) in
+            if let result = results?.first as? HKQuantitySample {
+                let height = result.quantity.doubleValue(for: HKUnit.meterUnit(with: .centi))
+                completion(height)
+            } else {
+                completion(nil)
+            }
+        }
+        manager.execute(query)
+    }
+    
+    func getBodyMass(completion: @escaping (Double?) -> Void) {
+        let query = HKSampleQuery(
+            sampleType: HKSampleType.quantityType(forIdentifier: .bodyMass)!,
+            predicate: nil,
+            limit: 1,
+            sortDescriptors: nil
+        ) { (query, results, error) in
+            if let result = results?.first as? HKQuantitySample {
+                let mass = result.quantity.doubleValue(for: HKUnit.gramUnit(with: .kilo))
+                completion(mass)
+            } else {
+                completion(nil)
+            }
+        }
+        manager.execute(query)
+    }
+    
+    //
+    func getSteps(start: Date, finish: Date, completion: @escaping (Double) -> Void) {
         let predicate = HKQuery.predicateForSamples(
-            withStart: startOfDay,
-            end: now,
+            withStart: start,
+            end: finish,
             options: .strictStartDate
         )
         let query = HKStatisticsQuery(
-            quantityType: stepsQuantityType,
+            quantityType: HKQuantityType(.stepCount),
             quantitySamplePredicate: predicate,
             options: .cumulativeSum
         ) { _, result, _ in
@@ -57,6 +102,77 @@ final class HealthKitManager: NSObject {
             }
             completion(sum.doubleValue(for: HKUnit.count()))
         }
+        manager.execute(query)
+    }
+    
+    //
+    func getHeartRate(start: Date, finish: Date, completion: @escaping (Double?) -> Void) {
+        let predicate = HKQuery.predicateForSamples(
+            withStart: start,
+            end: finish,
+            options: .strictStartDate)
+        let query = HKStatisticsQuery(
+            quantityType: HKQuantityType(.heartRate),
+            quantitySamplePredicate: predicate,
+            options: .discreteAverage
+        ) { query, results, error in
+            
+            print("result getHeartRate: \(results)")
+            
+            if results == nil {
+                completion(nil)
+            } else {
+                completion(999.999)
+            }
+        }
+        manager.execute(query)
+    }
+    
+    //
+    func getRunningStrideLength(start: Date, finish: Date, completion: @escaping (Double?) -> Void) {
+        let predicate = HKQuery.predicateForSamples(
+            withStart: start,
+            end: finish,
+            options: .strictStartDate)
+        let query = HKSampleQuery(
+            sampleType: HKSampleType.quantityType(forIdentifier: .runningStrideLength)!,
+            predicate: predicate,
+            limit: 1,
+            sortDescriptors: nil
+        ) { (query, results, error) in
+            
+            print("results runningStrideLength: \(results)")
+            
+//            if let result = results?.first as? HKQuantitySample {
+//                let height = result.quantity.doubleValue(for: HKUnit.meterUnit(with: .centi))
+//                completion(height)
+//            } else {
+//                completion(nil)
+//            }
+        }
+        manager.execute(query)
+    }
+
+    //
+    func getActiveEnergyBurned(start: Date, finish: Date, completion: @escaping (Double?) -> Void) {
+        
+        let predicate = HKQuery.predicateForSamples(
+            withStart: start,
+            end: finish,
+            options: [])
+        let query = HKSampleQuery(
+            sampleType: HKSampleType.quantityType(forIdentifier: .activeEnergyBurned)!,
+            predicate: predicate,
+            limit: 0,
+            sortDescriptors: nil,
+            resultsHandler: { (query, results, error) in
+                if let results = results {
+                    let listHKQuantitySample = results as! [HKQuantitySample]
+                    let values = listHKQuantitySample.map({ $0.quantity.doubleValue(for: HKUnit.kilocalorie()) })
+                    let kilocalorieTotal = values.reduce(0, +)
+                    completion(kilocalorieTotal)
+                }
+            })
         manager.execute(query)
     }
 }
