e8ab3472efb748f95415c04e83e056589d5a0a37
allDolinin <allDolinin@gmail.com>
2025-07-19 18:17:02 +0300

    add view description services

---
+++ Run/App/LocationDescriptionView.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+import Voyager
+import CoreLocation
+
+struct LocationDescriptionView: View {
+    
+    @EnvironmentObject var router: Router<AppRoute>
+    @State private var vm = ViewModel()
+        
+    var body: some View {
+        ZStack {
+            Color.purple
+                .opacity(0.8)
+            VStack(alignment: .leading, spacing: 24) {
+                Text("Тест про то зачем надо разрешить использовать геопозицию")
+                    .foregroundStyle(.white)
+                Button(action: {
+                    router.dismiss()
+                    LocationManager.shared.manager.requestWhenInUseAuthorization()
+                }) {
+                    Text("Ok")
+                        .foregroundStyle(.white)
+                        .frame(width: 200, height: 64)
+                }
+            }
+            .safeAreaPadding(.horizontal)
+            .safeAreaPadding(.bottom, 64)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    LocationDescriptionView()
+}
+
+
+extension LocationDescriptionView {
+    
+    @Observable
+    class ViewModel {
+        
+        
+    }
+}
